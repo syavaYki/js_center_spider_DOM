76f9c670c802a5080b19a7f484f2509221d87715
Seva Podolskiy <s_podolskiy@live.com>
2024-07-05 13:52:42 -0400

    make build

---
+++ index.44d7a322.js
@@ -0,0 +1,2 @@
+var l=document.querySelector(".wall"),e=document.querySelector(".spider");e.style.left=(null==l?void 0:l.clientWidth)/2-(null==e?void 0:e.clientWidth)/2+"px",e.style.top=(null==l?void 0:l.clientHeight)/2-(null==e?void 0:e.clientHeight)/2+"px";
+//# sourceMappingURL=index.44d7a322.js.map
